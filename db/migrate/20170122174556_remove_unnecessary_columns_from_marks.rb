322e737f2155e690d0c1bfd6b548a4a82c7a0b95
matilde2014 <edlitamguanarteme@gmail.com>
2017-01-22 21:11:34 +0100

    resolver problema de heroku

--- db/migrate/20170122174556_remove_unnecessary_columns_from_marks.rb
+++ db/migrate/20170122174556_remove_unnecessary_columns_from_marks.rb
@@ -1,7 +1,7 @@
 class RemoveUnnecessaryColumnsFromMarks < ActiveRecord::Migration
   def change
-    #remove_column :marks, :subject_id
-    #remove_column :marks, :description
-    #remove_column :marks, :mdate
+    remove_column :marks, :subject_id
+    remove_column :marks, :description
+    remove_column :marks, :mdate
   end
 end
